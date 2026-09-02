385def9105c3e45315ef28c2fe86ddea43c9b619
Jayden <cabaljayden@gmail.com>
2026-09-01 17:23:32 -0700

    added CORS

--- backend/app.py
+++ backend/app.py
@@ -6,10 +6,12 @@ from datetime import datetime, date
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from sqlalchemy import func
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)  
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
