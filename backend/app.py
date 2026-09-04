76a0b0ea497aaeea03b1a9b25394c7ed859413a4
Jayden <cabaljayden@gmail.com>
2026-09-04 15:06:24 -0700

    changes for deployment

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)  
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
 
